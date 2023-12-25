9655d747f5ef9e66bb5a5cac8c84c795fa50c5e4
Matthew McCall <m-mccall+git@outlook.com>
2023-12-25 14:28:23 -0500

    Resize GitHub calendar on small displays

--- src/components/GitHubCalendar.tsx
+++ src/components/GitHubCalendar.tsx
@@ -1,10 +1,36 @@
 'use client'
 
 import GitHubCalendar from "react-github-calendar";
-import {useContext} from "react";
-import {ThemeContext} from "@mmccalldev/components/ThemeProvider";
+import { useContext } from "react";
+import { ThemeContext } from "@mmccalldev/components/ThemeProvider";
+
+const selectLastHalfYear = (contributions: any[]) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 6;
+
+    return contributions.filter((activity: { date: string | number | Date; }) => {
+        const date = new Date(activity.date);
+        const monthOfDay = date.getMonth();
+
+        return (
+            date.getFullYear() === currentYear &&
+            monthOfDay > currentMonth - shownMonths &&
+            monthOfDay <= currentMonth
+        );
+    });
+};
 
 export default function GitHubCalendarWrapper() {
     let theme = useContext(ThemeContext)
-    return (<GitHubCalendar username={"matthew-mccall"} colorScheme={theme} />)
+    return (
+        <>
+            <div className="d-none d-md-block">
+                <GitHubCalendar username={"matthew-mccall"} colorScheme={theme} />
+            </div>
+            <div className="d-md-none">
+                <GitHubCalendar username={"matthew-mccall"} colorScheme={theme} transformData={selectLastHalfYear} />
+            </div>
+        </>
+    )
 }
