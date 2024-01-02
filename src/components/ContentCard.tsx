90b2711bf089944d39ec1d538cd1eb1263e38285
Matthew McCall <m-mccall+git@outlook.com>
2024-01-01 19:47:13 -0500

    Simplify card

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -2,7 +2,6 @@ import {Content} from "@mmccalldev/lib/Content";
 import React, {CSSProperties} from "react";
 import {Card} from "react-bootstrap";
 import Color from "colorjs.io"
-import Link from "next/link";
 
 export default function ContentCard({image, overlay, icon, color, title, description, link, date}: Content) {
 
@@ -12,16 +11,7 @@ export default function ContentCard({image, overlay, icon, color, title, descrip
         fontFeatureSettings: "'ss01', 'cv11', 'zero'",
     }
 
-    let linkElement = null;
-
-    if (link) {
-        // check if link is external
-        if (link.startsWith("http")) {
-            linkElement = <a href={link} className={"stretched-link text-reset text-decoration-none"}><Card.Title style={cardTitleStyle}>{title}</Card.Title></a>
-        } else {
-            linkElement = <Link href={link} className={"stretched-link text-reset text-decoration-none"}><Card.Title style={cardTitleStyle}>{title}</Card.Title></Link>
-        }
-    }
+    let linkElement = <a href={link} className={"stretched-link text-reset text-decoration-none"}><Card.Title style={cardTitleStyle}>{title}</Card.Title></a>
 
     const cardContent = (<>
         {link ? linkElement : <Card.Title style={cardTitleStyle}>{title}</Card.Title> }
@@ -34,25 +24,21 @@ export default function ContentCard({image, overlay, icon, color, title, descrip
         </Card.Text>
     </>)
 
-    if (overlay && image && color) {
+    if (image && color) {
         const backgroundColor = new Color(color);
         const lightForeground = new Color("white");
         const darkForeground = new Color("black");
 
         const theme = backgroundColor.contrastWCAG21(lightForeground) > backgroundColor.contrastWCAG21(darkForeground) ? "dark" : "light";
 
-        return (<Card className={"border-0 shadow"} data-bs-theme={theme}>
-            <Card.Img src={image} alt={title} />
-            <Card.ImgOverlay className={"d-flex flex-column justify-content-end"} style={{backgroundImage: `linear-gradient(transparent 33%, ${color})`}}>
-                {cardContent}
-            </Card.ImgOverlay>
-        </Card>)
-    } else if (image && color) {
-        const backgroundColor = new Color(color);
-        const lightForeground = new Color("white");
-        const darkForeground = new Color("black");
-
-        const theme = backgroundColor.contrastWCAG21(lightForeground) > backgroundColor.contrastWCAG21(darkForeground) ? "dark" : "light";
+        if (overlay) {
+            return (<Card className={"border-0 shadow"} data-bs-theme={theme}>
+                <Card.Img src={image} alt={title} />
+                <Card.ImgOverlay className={"d-flex flex-column justify-content-end"} style={{backgroundImage: `linear-gradient(transparent 33%, ${color})`}}>
+                    {cardContent}
+                </Card.ImgOverlay>
+            </Card>)
+        }
 
         return (<Card className={`border-0 shadow`} data-bs-theme={theme} style={{background: color}}>
             <Card.Img src={image} alt={title} variant={"top"} />
