3998b1b74d706b3fac406b677d4de0ca4adb37d3
Matthew McCall <m-mccall+git@outlook.com>
2024-01-03 22:08:19 -0500

    Switched to using a single GitHubCalendar

--- src/components/GitHubCalendar.tsx
+++ src/components/GitHubCalendar.tsx
@@ -1,36 +1,35 @@
 'use client'
 
 import GitHubCalendar from "react-github-calendar";
-import { useContext } from "react";
+import {useContext, useEffect, useState} from "react";
 import { ThemeContext } from "@mmccalldev/components/ThemeProvider";
+import {Activity} from "react-activity-calendar";
 
-const selectLastHalfYear = (contributions: any[]) => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 6;
+const selectLastHalfYear = (contributions: Array<Activity>) => {
+    // Check whether the contribution is in the last 6 months accounting for new years
+    const now = new Date()
+    const sixMonthsAgo = new Date(now.getFullYear(), now.getMonth() - 6, now.getDate())
 
-    return contributions.filter((activity: { date: string | number | Date; }) => {
-        const date = new Date(activity.date);
-        const monthOfDay = date.getMonth();
-
-        return (
-            date.getFullYear() === currentYear &&
-            monthOfDay > currentMonth - shownMonths &&
-            monthOfDay <= currentMonth
-        );
-    });
+    return contributions.filter((contribution) => {
+        const date = new Date(contribution.date)
+        return date.getTime() > sixMonthsAgo.getTime()
+    })
 };
 
 export default function GitHubCalendarWrapper() {
-    let theme = useContext(ThemeContext)
-    return (
-        <>
-            <div className="d-none d-md-block">
-                <GitHubCalendar username={"matthew-mccall"} colorScheme={theme} />
-            </div>
-            <div className="d-md-none">
-                <GitHubCalendar username={"matthew-mccall"} colorScheme={theme} transformData={selectLastHalfYear} />
-            </div>
-        </>
-    )
+
+    const theme = useContext(ThemeContext)
+    const [useLastHalfYear, setUseLastHalfYear] = useState(false)
+
+    // check whether the viewport is at least lg
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setUseLastHalfYear(window.innerWidth <= 992)
+            window.addEventListener('resize', () => {
+                setUseLastHalfYear(window.innerWidth <= 992)
+            })
+        }
+    }, [])
+
+    return <GitHubCalendar username={"matthew-mccall"} colorScheme={theme} transformData={useLastHalfYear ? selectLastHalfYear : undefined} hideColorLegend={useLastHalfYear} />
 }
