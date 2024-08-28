dc49af699c76cd8f2f1a9f61a1a624ba3e261fd4
Matthew McCall <m-mccall+git@outlook.com>
2024-08-28 11:56:16 -0400

    Add BlobBackground component and integrate into page
    Introduce the BlobBackground component to improve the visual design of the main page. This component generates a dynamic background with blobs using different color schemes. Updated the main page to integrate this background, enhancing the overall user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import GetYouTubeContent from "@mmccalldev/lib/YouTubeContent";
 import GetGitHubContent from "@mmccalldev/lib/GitHubContent";
 import GetTwitchContent from "@mmccalldev/lib/TwitchContent";
 import ContentGrid from "@mmccalldev/components/ContentGrid";
+import BlobBackground from "@mmccalldev/components/BlobBackground";
 
 async function getGitHubContent() {
     return Promise.all(await GetGitHubContent())
@@ -26,49 +27,53 @@ export default async function Home() {
     const socials: Map<string, string> = new Map([['github', 'https://github.com/matthew-mccall'], ['linkedin', 'https://www.linkedin.com/in/96d9/'], ['instagram', 'https://www.instagram.com/__mmccall/'], ['twitter-x', 'https://twitter.com/__mmccall'], ['youtube', 'https://www.youtube.com/@__mmccall'], ['twitch', 'https://www.twitch.tv/mmapptv'],]);
 
     return (<main>
-        <div className={'d-flex align-items-center'} style={{height: '67lvh'}}>
-            <Container className="text-center">
-                <h1 className={'display-1 fw-semibold'}>Matthew McCall</h1>
-                <p className={'lead'}>Computer Science and Computer Systems Engineering Dual Major at Rensselaer
-                    Polytechnic Institute</p>
-                <Stack direction={"horizontal"} gap={4} className={"d-inline-flex mx-auto"}>
-                    {[...socials].map(([name, url]) => (<a key={name} href={url} className={'fs-4'}>
-                        <i className={`bi-${name}`}/>
-                    </a>))}
+        <BlobBackground className={"bg-black text-light"} blobCount={10}>
+            <div className={'d-flex align-items-center'} style={{height: '67lvh'}}>
+                <Container className="text-center">
+                    <h1 className={'display-1 fw-semibold'}>Matthew McCall</h1>
+                    <p className={'lead'}>Computer Science and Computer Systems Engineering Dual Major at Rensselaer
+                        Polytechnic Institute</p>
+                    <Stack direction={"horizontal"} gap={4} className={"d-inline-flex mx-auto"}>
+                        {[...socials].map(([name, url]) => (<a key={name} href={url} className={'fs-4 text-reset'}>
+                            <i className={`bi-${name}`}/>
+                        </a>))}
+                    </Stack>
+                </Container>
+            </div>
+        </BlobBackground>
+        <div className={"bg-body pt-5"}>
+            <Container>
+                <Stack gap={3}>
+                    <h2>Projects</h2>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Open Algebra Software for Inferring Solutions</CardTitle>
+                            <CardText>Open Algebra Software for Inferring Solutions (OASIS) is a C++ library for embedding
+                                computer algebra and symbolic manipulation. I have led the development of the project since
+                                its initial pitch to the Rensselaer Center for Open Source. The project is under continuous
+                                active development and continues to introduce new features on a regular basis. I am also
+                                responsible for many of its subprojects including the web and desktop applications, and the
+                                C wrapper.</CardText>
+                            <CardLink href="https://oasis.mmccall.dev">Open Algebra Project Website</CardLink>
+                        </CardBody>
+                    </Card>
+                    <h2>Recent Activity</h2>
+                    <ContentGrid content={totalContent}/>
+                    <div>
+                        <h2>Fun Facts</h2>
+                        <p>Ah! So you scrolled all the way to the bottom. Well, here are some fun facts about me:</p>
+                        <ul>
+                            <li>Preferred programming language: C++</li>
+                            <li>
+                                Favorite games: Minecraft and Tetris
+                                <br />
+                                <span className={"fw-semibold"}>Bonus fact:</span> I&apos;ve made two plugins for the Paper Minecraft server.
+                            </li>
+                            <li>The &lsquo;96d9&rsquo; in my LinkedIn link is the first 4 letters of the SHA-1 checksum of my name: <code>echo Matthew McCall | shasum</code>.</li>
+                        </ul>
+                    </div>
                 </Stack>
             </Container>
         </div>
-        <Container>
-            <Stack gap={3}>
-                <h2>Projects</h2>
-                <Card>
-                    <CardBody>
-                        <CardTitle>Open Algebra Software for Inferring Solutions</CardTitle>
-                        <CardText>Open Algebra Software for Inferring Solutions (OASIS) is a C++ library for embedding
-                            computer algebra and symbolic manipulation. I have led the development of the project since
-                            its initial pitch to the Rensselaer Center for Open Source. The project is under continuous
-                            active development and continues to introduce new features on a regular basis. I am also
-                            responsible for many of its subprojects including the web and desktop applications, and the
-                            C wrapper.</CardText>
-                        <CardLink href="https://oasis.mmccall.dev">Open Algebra Project Website</CardLink>
-                    </CardBody>
-                </Card>
-                <h2>Recent Activity</h2>
-                <ContentGrid content={totalContent}/>
-                <div>
-                    <h2>Fun Facts</h2>
-                    <p>Ah! So you scrolled all the way to the bottom. Well, here are some fun facts about me:</p>
-                    <ul>
-                        <li>Preferred programming language: C++</li>
-                        <li>
-                            Favorite games: Minecraft and Tetris
-                            <br />
-                            <span className={"fw-semibold"}>Bonus fact:</span> I&apos;ve made two plugins for the Paper Minecraft server.
-                        </li>
-                        <li>The &lsquo;96d9&rsquo; in my LinkedIn link is the first 4 letters of the SHA-1 checksum of my name: <code>echo Matthew McCall | shasum</code>.</li>
-                    </ul>
-                </div>
-            </Stack>
-        </Container>
     </main>)
 }
---
+++ src/components/BlobBackground.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React, {HTMLProps} from "react";
+
+interface ColorScheme {
+    scheme: 'Complementary' | 'Analogous' | 'Triadic' | 'Split Complementary' | 'Tetradic' | 'Monochromatic';
+    hues?: number[];
+}
+
+export default function BlobBackground({children, className, blobCount = 5}: {
+    blobCount?: number
+} & HTMLProps<HTMLDivElement>)
+{
+    // pick random color scheme
+    const colorSchemes: ColorScheme[] = [
+        { scheme: 'Complementary' },
+        { scheme: 'Analogous' },
+        { scheme: 'Triadic' },
+        { scheme: 'Split Complementary' },
+        { scheme: 'Tetradic' },
+        { scheme: 'Monochromatic' }
+    ];
+
+    const colorScheme = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
+
+    // generate array of colors based on color scheme
+    const baseHue = Math.random() * 360;
+
+    switch (colorScheme.scheme) {
+        case 'Complementary':
+            colorScheme.hues = [baseHue, baseHue + 180];
+            break;
+        case 'Analogous':
+            colorScheme.hues = [baseHue, baseHue + 30, baseHue + 60];
+            break;
+        case 'Triadic':
+            colorScheme.hues = [baseHue, baseHue + 120, baseHue + 240];
+            break;
+        case 'Split Complementary':
+            colorScheme.hues = [baseHue, baseHue + 150, baseHue + 210];
+            break;
+        case 'Tetradic':
+            colorScheme.hues = [baseHue, baseHue + 90, baseHue + 180, baseHue + 270];
+            break;
+        case 'Monochromatic':
+            colorScheme.hues = [baseHue]
+            break;
+    }
+
+    // sample 10 colors from array
+    const sampledHues = [];
+    for (let i = 0; i < blobCount; i++) {
+        sampledHues.push(colorScheme.hues[Math.floor(Math.random() * colorScheme.hues.length)]);
+    }
+
+    return (
+        <div className={`position-relative ${className}`} style={{zIndex: -2}}>
+            <svg style={{
+                filter: "blur(72px)",
+            }} className={"z-n1 position-absolute w-100 h-100"}>
+                {
+                    sampledHues.map((hue, i) => {
+                        const lightness = colorScheme.scheme === 'Monochromatic' ? 50 : Math.random() * 50 + 25;
+                        return (<circle key={i} cx={`${Math.random() * 100}%`} cy={`${Math.random() * 100}%`} r={`${Math.random() * 100 + 72}`} fill={`hsla(${hue}, 100%, ${lightness}%, 0.75)`}/>)
+                    })
+                }
+            </svg>
+            {children}
+        </div>
+    )
+}
