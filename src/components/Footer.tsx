fa0e55a8fe4fc0b9e0c8cb08d89cb4605158629d
Matthew McCall <m-mccall+git@outlook.com>
2024-09-21 14:47:50 -0400

    Refactor Footer component to improve HTML structure.
    Reorganized CSS class assignments for better maintainability and consistency. No changes were made to the functionality or visual appearance of the Footer component.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { Container } from "react-bootstrap";
 
 export default function Footer() {
     return (
-        <footer>
-            <Container className="py-5 bg-body text-muted text-center">
+        <footer className={"bg-body"}>
+            <Container className="py-5 text-muted text-center">
                 <p>Copyright &copy; {new Date().getFullYear()} Matthew McCall. All rights reserved.</p>
                 <p>mmccall.dev is built on several open-source technologies including <a className={"text-reset"} href={"https://hammerjs.github.io/"}>hammer.js</a> and <a className={"text-reset"} href={"https://github.com/e-oj/Magic-Grid"}>e-oj/Magic-Grid</a>!</p>
             </Container>
