187c7facc1408185391921a23b7ac1ee45f7723c
Matthew McCall <m-mccall+git@outlook.com>
2024-10-26 18:41:19 -0400

    Replace BlobBackground with mesh gradient

--- src/components/BlobBackground/index.tsx
+++ src/components/BlobBackground/index.tsx
@@ -1,80 +1,31 @@
 'use client'
 
 import React, {HTMLProps, useEffect, useState} from "react";
-import style from "./style.module.css"
-
-interface ColorScheme {
-    scheme: 'Complementary' | 'Analogous' | 'Triadic' | 'Split Complementary' | 'Tetradic';
-    hues?: number[];
-}
+import {MeshGradientRenderer} from "@johnn-e/react-mesh-gradient";
 
 interface BlobBackgroundState {
     sampledHues: number[];
 }
 
-export default function BlobBackground({children, className, blobCount = 7}: {
+export default function BlobBackground({children, className, blobCount = 5}: {
     blobCount?: number
 } & HTMLProps<HTMLDivElement>)
 {
     const [state, setState] = useState<BlobBackgroundState | null>(null);
 
-    useEffect(() => {
-        // pick random color scheme
-        const colorSchemes: ColorScheme[] = [
-            { scheme: 'Complementary' },
-            { scheme: 'Analogous' },
-            { scheme: 'Triadic' },
-            { scheme: 'Split Complementary' },
-            { scheme: 'Tetradic' },
-        ];
+    const pickNewHues = () => {
+        const baseHue = Math.floor(Math.random() * 360);
+        const hueStep = 10;
+        const analogousHues = Array.from({length: 5}, (_, i) => (baseHue + i * hueStep) % 360);
+        setState({sampledHues: analogousHues});
+    }
 
-        const colorScheme = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
-
-        // generate array of colors based on color scheme
-        const baseHue = Math.random() * 360;
-
-        switch (colorScheme.scheme) {
-            case 'Complementary':
-                colorScheme.hues = [baseHue, baseHue + 180];
-                break;
-            case 'Analogous':
-                colorScheme.hues = [baseHue, baseHue + 30, baseHue + 60];
-                break;
-            case 'Triadic':
-                colorScheme.hues = [baseHue, baseHue + 120, baseHue + 240];
-                break;
-            case 'Split Complementary':
-                colorScheme.hues = [baseHue, baseHue + 150, baseHue + 210];
-                break;
-            case 'Tetradic':
-                colorScheme.hues = [baseHue, baseHue + 90, baseHue + 180, baseHue + 270];
-                break;
-        }
-
-        const sampledHues = [];
-        for (let i = 0; i < blobCount; i++) {
-            sampledHues.push(colorScheme.hues[Math.floor(Math.random() * colorScheme.hues.length)]);
-        }
-
-        setState({ sampledHues });
-    }, [])
+    useEffect(() => { pickNewHues() }, [])
 
     return (
-        <div className={"position-relative"}>
-            <div className={`position-fixed z-n1 vh-100 w-100 ${className} top-0`} style={{backgroundColor: `hsl(${state?.sampledHues[0]}, 50%, 10%)`}}>
-                {state && <svg style={{
-                    filter: "blur(72px)",
-                }} className={`w-100 h-100 ${style.fadeIn}`}>
-                    {
-                        state.sampledHues.map((hue, i) => {
-                            return (<circle key={i} cx={`${Math.random() * 100}%`} cy={`${Math.random() * 100}%`}
-                                            r={`${Math.random() * 33 + 33}%`}
-                                            fill={`hsla(${hue}, 90%, 60%, 30%`}/>)
-                        })
-                    }
-                </svg>}
-            </div>
+        <>
+            {state && <MeshGradientRenderer className={"vh-100 w-100"} colors={state.sampledHues.map(hue => `hsl(${hue}, 66%, 30%)`)} onGradientClick={pickNewHues} speed={0.005} />}
             {children}
-        </div>
+        </>
     )
 }
