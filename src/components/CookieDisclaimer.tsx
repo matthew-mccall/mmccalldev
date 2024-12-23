c15b090e591014778304ba4060edb6366977d871
Matthew McCall <m-mccall+git@outlook.com>
2024-12-23 10:49:55 -0500

    Update styles for headings and card appearance
    Removed unnecessary comment, adjusted `h2` border thickness and spacing, and refined global styles. These changes improve visual hierarchy and consistency in the UI.

--- src/components/CookieDisclaimer.tsx
+++ src/components/CookieDisclaimer.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import React, {useState} from "react";
-import Script from "next/script";
+import Clarity from "@microsoft/clarity";
 import {Toast, ToastBody, ToastContainer, ToastHeader} from "react-bootstrap";
 import AcrylicStyle from "@mmccalldev/styles/Acrylic.module.css";
 
 export default function CookieDisclaimer() {
     const [showCookieState, setShowCookieState] = useState<boolean>(true)
+    Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID!);
 
     return (<>
         <ToastContainer className={"p-3 z-1 position-fixed"} position={"bottom-end"}>
@@ -18,14 +19,5 @@ export default function CookieDisclaimer() {
                     Privacy Statement</a>.</ToastBody>
             </Toast>
         </ToastContainer>
-        <Script id="clarity-script" strategy="afterInteractive">
-            {`
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-               `}
-        </Script>
     </>)
 }
