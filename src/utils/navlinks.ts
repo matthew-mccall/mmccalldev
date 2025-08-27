b1452a54bbef7f7465629f0345103d6feb8aa7d1
Matthew McCall <m-mccall+git@outlook.com>
2025-08-27 19:16:22 -0400

    Extract navigation links and socials into reusable utility modules; add footer with links and socials. Also remove redundant inline link styles and improve spacing.

---
+++ src/utils/navlinks.ts
@@ -0,0 +1,8 @@
+const navLinks = [
+    { link: '/', title: 'home' },
+    { link: '/posts', title: 'posts' },
+    { link: '/papers', title: 'papers' },
+    { link: '/resume.pdf', title: 'resume' },
+];
+
+export default navLinks;
