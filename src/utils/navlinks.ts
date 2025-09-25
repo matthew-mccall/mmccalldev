e8a232aafdbb4a8ca923e9bfd694fe2b5b02fbe4
Matthew McCall <m-mccall+git@outlook.com>
2025-09-25 01:00:33 -0400

    Add icons to navigation links; update `NavigationBar` and `navlinks` structure for improved visuals

--- src/utils/navlinks.ts
+++ src/utils/navlinks.ts
@@ -1,8 +1,8 @@
 const navLinks = [
-    { link: '/', title: 'home' },
-    { link: '/posts', title: 'posts' },
-    { link: '/papers', title: 'papers' },
-    { link: '/resume.pdf', title: 'resume' },
+    { link: '/', title: 'home', icon: 'house-door' },
+    { link: '/posts', title: 'posts', icon: 'newspaper' },
+    { link: '/papers', title: 'papers', icon: 'journal-bookmark' },
+    { link: '/resume.pdf', title: 'resume', icon: 'filetype-pdf' },
 ];
 
 export default navLinks;
